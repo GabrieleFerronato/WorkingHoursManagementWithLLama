35dcc4e1d5313eb438802f5d10e61ff12f6b5a6c
Gabriele Ferronato <ferronatogabriele6@gmail.com>
2024-01-09 17:19:37 +0100

    sistemato baco nella funzione DoRequest

--- LLamaApiPerSceltaParametri/Libs/Request.py
+++ LLamaApiPerSceltaParametri/Libs/Request.py
@@ -1,115 +1,19 @@
 from Params.params import Getkey
+import json
 #fa la richiesta all'api di LLama
 def DoRequest(Attivita):
     from llamaapi import LlamaAPI
     llama = LlamaAPI(Getkey())
 
+    #read the data from the json file
+    with open("json.json") as json_file:
+        data = json.load(json_file)
+
+    text = '''dato il seguente json e un input da utente che indica l'attività che ha svolto in giornata, ritorna solo il dato che potrebbe indicare meglio la sua attività,
+        non argomentare, ritorna solo il dato tra #..#, il json è:''' + str(data) + '''l'input dell'utente è:''' + Attivita
+
     api_request_json = {
-        "messages":[{"role": "user", "content": '''
-        dato il seguente json e un input da utente che indica l'attività che ha svolto in giornata, ritorna solo il dato che potrebbe indicare meglio la sua attività,
-        non argomentare, ritorna solo il dato tra #..#, il json è:{
-        "data":{
-            "TMG Text Translator":{
-                "Progetto":"Y100-PRJ012",
-                "Ordine Destinatario":"",
-                "Network":"",
-                "Operazione":"",
-                "Causa":"I020",
-                "Ts":""
-            },
-            "Cap6Maker":{
-                "Progetto":"Y100-PRJ013",
-                "Ordine Destinatario":"",
-                "Network":"",
-                "Operazione":"",
-                "Causa":"I020",
-                "Ts":""
-            },
-            "MesInterface":{
-                "Progetto":"Y100-PRJ014",
-                "Ordine Destinatario":"",
-                "Network":"",
-                "Operazione":"",
-                "Causa":"I020",
-                "Ts":""
-            },
-            "Pannelli Unified":{
-                "Progetto":"Y100-PRJ015",
-                "Ordine Destinatario":"",
-                "Network":"",
-                "Operazione":"",
-                "Causa":"I020",
-                "Ts":""
-            },
-            "Energy Meter":{
-                "Progetto":"Y100-PRJ018",
-                "Ordine Destinatario":"",
-                "Network":"",
-                "Operazione":"",
-                "Causa":"I020",
-                "Ts":""
-            },
-            "Assistenza Generica Cliente":{
-                "Progetto":"9018",
-                "Ordine Destinatario":"",
-                "Network":"",
-                "Operazione":"",
-                "Causa":"I018",
-                "Ts":""
-            },
-            "Corsi di formazione":{
-                "Progetto":"9013",
-                "Ordine Destinatario":"",
-                "Network":"",
-                "Operazione":"",
-                "Causa":"I018",
-                "Ts":""
-            },
-            "Supporto altri reparti (colleghi)":{
-                "Progetto":"9015",
-                "Ordine Destinatario":"",
-                "Network":"",
-                "Operazione":"",
-                "Causa":"I015",
-                "Ts":""
-            },
-            "Progetto Optix P&G Polonia":{
-                "Progetto":"4071277",
-                "Ordine Destinatario":"",
-                "Network":"",
-                "Operazione":"10",
-                "Causa":"N013",
-                "Ts":""
-            },
-            "Supervisione Generale":{
-                "Progetto":"4055977",
-                "Ordine Destinatario":"",
-                "Network":"",
-                "Operazione":"10",
-                "Causa":"N013",
-                "Ts":""
-            },
-            "Collaudo Supervisione Tecnobox":{
-                "Progetto":"4068932",
-                "Ordine Destinatario":"",
-                "Network":"",
-                "Operazione":"20",
-                "Causa":"N006",
-                "Ts":""
-            },
-            "Sviluppo SW Supervisione Tecnobox":{
-                "Progetto":"4068932",
-                "Ordine Destinatario":"",
-                "Network":"",
-                "Operazione":"10",
-                "Causa":"N013",
-                "Ts":""
-            }
-        }
-    },
-
-    l'input dell'utente è:''' + Attivita}],
-
+        "messages":[{"role": "user", "content": text}],
         }
 
     response = llama.run(api_request_json)
